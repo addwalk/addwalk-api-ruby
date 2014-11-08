283d7e40022ab7200890e56867a44fa948643759
Frank Cieslik <frank.cieslik@gmail.com>
2014-11-08 22:03:49 +0000

    added test for single product informations

--- spec/models/product.rb
+++ spec/models/product.rb
@@ -26,6 +26,20 @@ describe "Addwalk::Product" do
     end
   end
 
+  it "get a simple product" do
+    # get token
+    products_request = Addwalk::Product.new(@service_provider.token).index()
+    expect(products_request[:success]).to eq(true)
+    expect(products_request[:products].size).to eq(10)
+    product_id = products_request[:products].first[:id].to_i
+    expect(product_id).to be > 0
+
+    # call product
+    product_request = Addwalk::Product.new(@service_provider.token).show(product_id)
+    expect(product_request[:success]).to eq(true)
+    expect(product_request[:product][:id].to_i).to eq(product_id)
+  end
+
   xit "finds product" do
     search_for_category = "area_apparel"
     category_request = Addwalk::Category.new(@service_provider.token).index(identifier: search_for_category)
